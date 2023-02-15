a75488d4de39b3a1e35f328289a743c89731e839
aramis-matos <aramis.matos1@gmail.com>
2023-02-15 07:52:24 -0400

    fixed things

--- asignment_3/fonosspanish.pl
+++ asignment_3/fonosspanish.pl
@@ -1,44 +1,46 @@
 :- [fullanswerMatosAramis].
 
-fono(X,[cns]) :- atom_codes(X,[112]).
-fono(X,[cns, son]) :- atom_codes(X,[98]).
-fono(X,[cns, ant, dst]) :- atom_codes(X, [116,810]).
-fono(X,[cns, son, ant, dst]) :- atom_codes(X,[100,810]).
-fono(X,[cns,alt,rtr]) :- atom_codes(X,[107]).
-fono(X,[cns,son,alt,rtr]) :- atom_codes(X,[103]).
-fono(X,[cns, cnt, son]) :- atom_codes(X,[946]).
-fono(X,[cns,cnt,str]) :- atom_codes(X,[102]).
-fono(X,[cns,cnt,ant,dst]) :- atom_codes(X,[952]).
-fono(X,[cns,cnt,son,ant,dst]) :- atom_codes(X,[240]).
-fono(X,[cns,cnt,str,ant]) :- atom_codes(X,[115]).
-fono(X,[cns,cnt,str,son,ant]) :- atom_codes(X,[122]).
-fono(X,[cns,cns,str,dst]) :- atom_codes(X,[643]).
-fono(X,[cns,cns,str,son,dst]) :- atom_codes(X,[658]).
-fono(X,[cns,cnt,son,dst,alt]) :- atom_codes(X,[669]).
-fono(X,[cns,cnt,str,alt,rtr]) :- atom_codes(X,[120]).
-fono(X,[cns,cnt,son,alt,rtr]) :- atom_codes(X,[611]).
+natClasses([cns,rsn,sil,cnt,str,lat,son,gle,rla,rlr,nas,lab,rnd,cor,ant,dst,dor,alt,baj,rtr]).
+
+fono(X,[cns,lab]) :- atom_codes(X,[112]).
+fono(X,[cns, son,lab]) :- atom_codes(X,[98]).
+fono(X,[cns, cor, ant, dst]) :- atom_codes(X, [116,810]).
+fono(X,[cns, son, cor, ant, dst]) :- atom_codes(X,[100,810]).
+fono(X,[cns,dor,alt,rtr]) :- atom_codes(X,[107]).
+fono(X,[cns,son,dor,alt,rtr]) :- atom_codes(X,[103]).
+fono(X,[cns, cnt, son,lab]) :- atom_codes(X,[946]).
+fono(X,[cns,cnt,str,lab]) :- atom_codes(X,[102]).
+fono(X,[cns,cnt,cor,ant,dst]) :- atom_codes(X,[952]).
+fono(X,[cns,cnt,son,cor,ant,dst]) :- atom_codes(X,[240]).
+fono(X,[cns,cnt,str,cor,ant]) :- atom_codes(X,[115]).
+fono(X,[cns,cnt,str,son,cor,ant]) :- atom_codes(X,[122]).
+fono(X,[cns,cns,str,cor,dst]) :- atom_codes(X,[643]).
+fono(X,[cns,cns,str,son,cor,dst]) :- atom_codes(X,[658]).
+fono(X,[cns,cnt,son,cor,dst,cor,alt]) :- atom_codes(X,[669]).
+fono(X,[cns,cnt,str,dor,alt,rtr]) :- atom_codes(X,[120]).
+fono(X,[cns,cnt,son,dor,alt,rtr]) :- atom_codes(X,[611]).
 fono(X,[cnt,gle]) :- atom_codes(X,[104]).
-fono(X,[cns,str,dst]) :- atom_codes(X,[679]).
-fono(X,[cns,str,son,dst]) :- atom_codes(X,[676]).
-fono(X,[cns,rsn,son,nas]) :- atom_codes(X,[109]).
-fono(X,[cns,rsn,cnt,son,nas]) :- atom_codes(X,[625]).
-fono(X,[cns,rsn,son,nas,ant,dst]) :- atom_codes(X,[110,810]).
-fono(X,[cns,rsn,son,nas,ant]) :- atom_codes(X,[110]).
-fono(X,[cns,rsn,son,nas,dst,alt]) :- atom_codes(X,[626]).
-fono(X,[cns,rsn,son,nas,alt,rtr]) :- atom_codes(X,[331]).
-fono(X,[cns,rsn,cnt,lat,son,ant,dst]) :- atom_codes(X,[108,810]).
-fono(X,[cns,rsn,cnt,lat,son,ant]) :- atom_codes(X,[108]).
-fono(X,[cns,rsn,cnt,lat,son,dst,alt]) :- atom_codes(X,[654]).
-fono(X,[cns,rsn,cnt,son,ant]) :- atom_codes(X,[114]).
-fono(X,[cns,rsn,cnt,son,rla,ant]) :- atom_codes(X,[638]).
-fono(X,[rsn,cnt,son,dst,alt]) :- atom_codes(X,[106]).
-fono(X,[rsn, cnt, son, rnd, alt, rtr]) :- atom_codes(X,[119]).
-fono(X,[rsn,sil,cnt,son,rla,alt]) :- atom_codes(X,[105]).
-fono(X,[rsn,sil,cnt,son,rla]) :- atom_codes(X,[101]).
-fono(X,[rsn,sil,cnt,son,rla,baj,rtr]) :- atom_codes(X,[97]).
-fono(X,[rsn,sil,cnt,son,rla,rnd,rtr]) :- atom_codes(X,[111]).
-fono(X,[rsn,sil,cnt,son,rnd,alt,rtr]) :- atom_codes(X,[117]).
-fono(X,[rsn,sil,cnt,son,rlr,rtr]) :- atom_codes(X,[601]).
+fono(X,[cns,str,cor,dst]) :- atom_codes(X,[679]).
+fono(X,[cns,str,son,cor,dst]) :- atom_codes(X,[676]).
+fono(X,[cns,rsn,son,nas,lab]) :- atom_codes(X,[109]).
+fono(X,[cns,rsn,cnt,son,nas,lab]) :- atom_codes(X,[625]).
+fono(X,[cns,rsn,son,nas,cor,ant,dst]) :- atom_codes(X,[110,810]).
+fono(X,[cns,rsn,son,nas,cor,ant]) :- atom_codes(X,[110]).
+fono(X,[cns,rsn,son,nas,cor,dst,dor,alt]) :- atom_codes(X,[626]).
+fono(X,[cns,rsn,son,nas,dor,alt,rtr]) :- atom_codes(X,[331]).
+fono(X,[cns,rsn,cnt,lat,son,cor,ant,dst]) :- atom_codes(X,[108,810]).
+fono(X,[cns,rsn,cnt,lat,son,cor,ant]) :- atom_codes(X,[108]).
+fono(X,[cns,rsn,cnt,lat,son,cor,dst,dor,alt]) :- atom_codes(X,[654]).
+fono(X,[cns,rsn,cnt,son,cor,ant]) :- atom_codes(X,[114]).
+fono(X,[cns,rsn,cnt,son,rla,cor,ant]) :- atom_codes(X,[638]).
+fono(X,[rsn,cnt,son,cor,dst,dor,alt]) :- atom_codes(X,[106]).
+fono(X,[rsn,cnt,son,lab,rnd,dor,alt,rtr]) :- atom_codes(X,[119]).
+fono(X,[rsn,sil,cnt,son,rla,dor,alt]) :- atom_codes(X,[105]).
+fono(X,[rsn,sil,cnt,son,rla,dor]) :- atom_codes(X,[101]).
+fono(X,[rsn,sil,cnt,son,rla,dor,baj,rtr]) :- atom_codes(X,[97]).
+fono(X,[rsn,sil,cnt,son,rla,lab,rnd,dor,rtr]) :- atom_codes(X,[111]).
+fono(X,[rsn,sil,cnt,son,lab,rnd,dor,alt,rtr]) :- atom_codes(X,[117]).
+fono(X,[rsn,sil,cnt,son,rlr,dor,rtr]) :- atom_codes(X,[601]).
 
 allphones(A,B) :- findall(A,fono(A,_),B).
 
@@ -57,8 +59,14 @@ fonos(Fonos,Total) :-
     Fonos = B,
     length(Fonos,Total).
 
+fono(X,Rpos,Rneg) :-
+    fono(X,Y),
+    members(Rpos,Y),
+    nonmembers(Y,Rneg).
 
 
-
-
-
+fonos(Fono,Rpos,Rneg) :- 
+    natClasses(X),
+    subtract(X, Rpos, Rneg),
+    fono(Fono,Rpos).
+    
